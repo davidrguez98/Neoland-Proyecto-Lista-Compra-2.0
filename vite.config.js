7cf02b4c1cb3b8c09a5373ddc77b81a36d93d45e
davidrguez98 <ropeda98@gmail.com>
2025-07-09 20:27:18 +0200

    prueba pwa ok

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa'
 import { resolve } from 'path';
-// import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
     root: '.',              // Carpeta base
@@ -49,4 +48,3 @@ export default defineConfig({
         open: true      // Abre el navegador al iniciar
     },
 });
-
