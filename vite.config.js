522310f121573b7cff911ecd30e74390de970dc5
davidrguez98 <ropeda98@gmail.com>
2025-07-09 20:18:54 +0200

    configuracion principal pwa

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
 import { resolve } from 'path';
 // import { VitePWA } from 'vite-plugin-pwa'
 
@@ -16,52 +17,29 @@ export default defineConfig({
         },
     },
     plugins: [
-        // VitePWA({
-        //     registerType: 'autoUpdate',
-        //     manifest: {
-        //         name: 'Sistema de Stock y Ventas',
-        //         short_name: 'StockApp',
-        //         start_url: '/',
-        //         display_override: ['window-controls-overlay'],
-        //         lang: 'es-ES',
-        //         display: 'standalone',
-        //         background_color: '#ffffff',
-        //         theme_color: '#1d4ed8',
-        //         screenshots: [
-        //             {
-        //                 "src": "screenshots/screenshot-desktop.png",
-        //                 "sizes": "1920x1080",
-        //                 "form_factor": "wide",
-        //                 "label": "Desktop view showing weekly meal calendar"
-        //             },
-        //             {
-        //                 "src": "screenshots/screenshot-mobile.png",
-        //                 "sizes": "750x1334",
-        //                 "form_factor": "narrow",
-        //                 "label": "Mobile view showing weekly meal calendar"
-        //             }
-        //         ],
-        //         icons: [
-        //             {
-        //                 src: 'pwa-64x64.png',
-        //                 sizes: '64x64',
-        //                 type: 'image/png',
-        //             },
-        //             {
-        //                 src: 'pwa-192x192.png',
-        //                 sizes: '192x192',
-        //                 type: 'image/png',
-        //                 purpose: 'any',
-        //             },
-        //             {
-        //                 src: 'pwa-512x512.png',
-        //                 sizes: '512x512',
-        //                 type: 'image/png',
-        //                 purpose: 'maskable',
-        //             },
-        //         ],
-        //     }
-        // })
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Sistema de venta de cafe',
+                short_name: 'StockApp',
+                start_url: '/',
+                display: 'standalone',
+                background_color: '#ffffff',
+                theme_color: '#1d4ed8',
+                icons: [
+                    {
+                        src: '/icons/icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/icons/icon-512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
     ],
     resolve: {
         extensions: ['.ts', '.js']
@@ -71,3 +49,4 @@ export default defineConfig({
         open: true      // Abre el navegador al iniciar
     },
 });
+
